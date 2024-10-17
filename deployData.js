8c58a15ffe46f7cf27ea340a9b5cefaf01f71b5b
Efrain Yaphet Obert <efrainzhenwu@gmail.com>
2024-10-17 13:36:08 +0700

    EditBUtton Error

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Efrain Yaphet Obert'; // TODO: replace this
+
+const studentId = 'FS10922243'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
