9712889582f59082ff4faa968d852f08dae849b1
Efrain Yaphet Obert <efrainzhenwu@gmail.com>
2024-10-17 14:43:16 +0700

    Deploy Link

--- deployData.js
+++ deployData.js
@@ -8,8 +8,8 @@
 
 const studentName = 'Efrain Yaphet Obert'; // TODO: replace this
 
-const studentId = 'FS10922243'; // TODO: replace this
+const studentId = 'FS03'; // TODO: replace this
 
-const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+const NetlifyDeployUrl = 'https://willowy-pie-4d39ef.netlify.app/'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
